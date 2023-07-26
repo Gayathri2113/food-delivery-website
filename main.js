04e9a8a970c5e88ec1b776ffc6766a31d3faa1f4
Gayathri2113 <gayathribhukya211@gmail.com>
2023-07-26 23:37:37 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let i = "Gaya"
+let h = 3
+console.log(i + h)
+const t = {
+    name: "Gayathri",
+    roll: 19
+}
+console.log(t['roll'])
+t['rolll'] = 20
+console.log(t['rolll'])
+t['Brother'] = 'Idiot'
+console.log(t)
+console.log(t.name)
+let heading = document.querySelector(".h-primary");
+heading.innerHTML = "Hello";
